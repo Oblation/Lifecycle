58f01110c9d0882c0fcc7283c73bdd82af9d5fec
Oblation <max.stru4kov@gmail.com>
2021-12-03 21:15:34 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, DoCheck, AfterContentChecked, AfterContentInit, AfterViewInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Lifecycle';
+export class AppComponent implements
+  OnChanges,
+  OnInit,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked {
+
+    title = 'hrgsd'
+
+  company: string = 'Google Inc.'
+
+  ngOnChanges() {
+    console.log('OnChanges')
+  }
+
+  ngOnInit() {
+    console.log('OnInit', this.company)
+  }
+
+  ngDoCheck() {
+    console.log('DoCheck')
+  }
+
+  ngAfterContentInit() {
+    console.log('AfterContentInit')
+  }
+
+  ngAfterContentChecked() {
+    console.log('AfterContentChecked')
+  }
+
+  ngAfterViewInit() {
+    console.log('AfterViewInit')
+  }
+
+  ngAfterViewChecked() {
+    console.log('AfterViewChecked')
+  }
 }
